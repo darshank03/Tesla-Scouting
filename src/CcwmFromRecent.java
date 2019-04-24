85621f0b7410ad5d1338e4ac8b210e3f407f27c2
Darshan <darsh797@gmail.com>
2019-04-23 20:06:00 -0400

    Added support for CCWM values from worlds instead of just previous competitions (but can only be used after every team has played at least one match)

---
+++ src/CcwmFromRecent.java
@@ -0,0 +1,244 @@
+import com.cpjd.main.TBA;
+import com.cpjd.models.events.Event;
+import com.cpjd.models.events.EventOPR;
+import com.cpjd.models.teams.Team;
+
+
+class CcwmFromRecent {
+    static void ccwmRecent(){
+
+            Object [][] teamStatArray = new Object[68][2];
+
+            TBA.setAuthToken("GtPgt3KxWPRM3VHt6H7ydrUcT8RXQy69Dk12p08I1uqzDevaGbzfqOuq9nwYqy5T");
+            TBA tba = new TBA();
+            Team[] tesla = tba.getEventTeams("2019tes");
+
+            try{
+                for (int i = 0; i < 8; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event[] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 8; i < 9; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-3;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for (int i = 9; i < 13; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 13; i < 14; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event recentEvent = tba.getEvent("2019necmp");
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 14; i < 15; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for (int i = 15; i < 33; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 33; i < 34; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-3;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for (int i = 34; i < 44; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 44; i < 45; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-3;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for (int i = 45; i < 47; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for(int i = 47; i < 48; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-3;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+                for (int i = 48; i < tesla.length; i++){
+                    Team team = tesla[i];
+                    int teamNumber = Math.toIntExact(team.getTeamNumber());
+                    Event [] teamEvents = tba.getTeamEvents(teamNumber);
+                    int recentEventIndex = teamEvents.length-2;
+                    Event recentEvent = teamEvents[recentEventIndex];
+                    String eventName = recentEvent.getKey();
+                    EventOPR[] oprs =  tba.getOprs(eventName);
+                    for (EventOPR opr : oprs){
+                        if(Main.keyToNum(opr.getTeamKey()) == teamNumber){
+                            Object[] teamStat = new Object[2];
+                            teamStat[0] = Main.keyToNum(opr.getTeamKey());
+                            teamStat[1] = opr.getCcwm();
+                            teamStatArray[i] = teamStat;
+                        }
+                    }
+                }
+
+                for(int i=0; i < teamStatArray.length; i++){
+                    for(int j=0; j < teamStatArray.length-i-1; j++){
+                        if(Double.parseDouble(teamStatArray[j][1].toString()) < Double.parseDouble(teamStatArray[j+1][1].toString())){
+                            Object [] tmp = teamStatArray[j];
+                            teamStatArray[j] = teamStatArray[j+1];
+                            teamStatArray[j+1] = tmp;
+                        }
+                    }
+                }
+                for(Object [] h : teamStatArray){
+                    for(Object g:h){
+                        System.out.println(g);
+                    }
+                }
+
+            }
+
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+
+        }
+
+}
